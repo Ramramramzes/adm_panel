beaeafd2e62c072b156c3d0b9edace3edcf242b9
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-02-25 16:17:56 +0300

    send stats

--- src/MainPanel/Main.tsx
+++ src/MainPanel/Main.tsx
@@ -1,15 +1,14 @@
-import { useEffect } from 'react';
+
 import { useLocation } from 'react-router-dom';
 
 export function Main() {
   const location = useLocation();
-  const {name: adminName , token: adminToken}:{name:string,token:string} = location.state && location.state.currentAdmin;
-  useEffect(()=>{
-    console.log(adminName,adminToken);
-  },[])
+  const {name: admName, token: admToken} = location.state;
+  console.log(admName,admToken);
+  
   
 
   return (
-    <div>Добро пожаловать {adminName && adminName}</div>
+    <div>Добро пожаловать {admName && admName}</div>
   );
 }
