4aa684d72ddf0c1fc675e6a344bfc9d39d7315ac
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-02-29 12:36:37 +0300

    comments what to do

--- src/Layout/Person/Person.tsx
+++ src/Layout/Person/Person.tsx
@@ -3,22 +3,22 @@
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
-interface IComments{
-  adm: string,
-  status: number,
-  comments: string,
-}
+// interface IComments{
+//   adm: string,
+//   status: number,
+//   comments: string,
+// }
 
 export function Person() {
   const location = useLocation();
   const navigate = useNavigate()
-  // const [comments,setComments] = useState({})
-  // setComments({'0': {adm: 'Main', status: 0, comments: 'bad'}})
 
-  //! Cначала реализовать кнопку добавления комментария 
-  //? Наверное попап
+  const comments = JSON.parse(location.state.comments)
 
-  console.log(JSON.parse(location.state.comments));  
+  console.log(location.state);
+  
+  const commentWorkArr = Object.values<string>(comments).map((el:string) => {return JSON.parse(el)});
+  //? console.log(Object.values<string>(comments).map((el:string) => {return JSON.parse(el)}));
   
 
   
@@ -31,8 +31,13 @@ export function Person() {
   return (
     <div>
       <h1>Сотрудник </h1>
+      <button>Добавить отзыв</button>
       <div>
-        <div>Комментарий от </div>
+        {comments && commentWorkArr.map((el, index) => {
+          if (el.adm != undefined){
+            return <div key={index}>{el.adm + ' - ' + el.comment}</div>
+          }
+        })}
       </div>
       <button onClick={handleClickBack}>На главную</button>
     </div>
@@ -40,5 +45,10 @@ export function Person() {
 }
 
 
-// UPDATE worker SET comments = JSON_SET(comments, '$.adm', 'adm', '$.status', 0, '$.comment', 'плохой') WHERE `name` = 'Marat'
+//? Создать попап с помощью portal
+//? В этот portal компонент передавать location.state.name и location.state.points 
+//? Сформировать шаблон как будет выглядеть 1 блок {} комментария (время / id для удаления)
+//? Сформировать запрос для добавления коммента (...Json) с деструктуризацие старого
+
+//?    UPDATE worker SET comments = JSON_SET(comments, '$."0"', '{"adm": "adm", "status": 0, "comment": "плохой"}', '$."1"', '{}') WHERE `name` = 'test';
 
